b272c3f2cc05caf6a7db03491ec292a9034226ff
Alexis Jhay Rosales <50823115+alexisjhay@users.noreply.github.com>
2026-09-09 05:47:46 +0800

    feat: Add calculator functionality
    - Update operate function
    - Add arithmetic operators
    - Calculate results
    - Add additional result display top for
    calculation and bottom for result
    - Handle invalid input
    - Add decimal support
    - Add clear functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,35 +1,158 @@
-function addition(total, currentNum){
-    total = total + currentNum;
-}
-
-function subtraction(total, currentNum){
-    total = total - currentNum;
-}
-
-function multiplication(total, currentNum){
-    total = total * currentNum;
-}
-
-function division(total, currentNum){
-    total = total / currentNum;
-}
-
-function operate(){
+function operate(previousNum, currentNum, operator){
     switch(operator){
         case "+":
-            addition(total, currentNum)
-            break;
+            result.textContent = +previousNum + +currentNum    
+            return +previousNum + +currentNum      
+        
         case "-":
-            subtraction(total, currentNum)
-            break;
+            result.textContent = +previousNum - +currentNum    
+            return +previousNum - +currentNum       
+
         case "x":
-            multiplication(total, currentNum)
-            break;
+            result.textContent = +previousNum * +currentNum    
+            return +previousNum * +currentNum  
+           
         case "÷":
-            division(total, currentNum)
-            break;
+            result.textContent = +previousNum / +currentNum    
+            return +previousNum / +currentNum 
     }
 }
 
-let total = null;
+let result = document.querySelector(".bottom-res");
+result.textContent = "0";
+let topResult = document.querySelector(".top-res")
+
+let currentNum = result.textContent;
+let previousNum = null;
 let operator;
+let justCalculated = false;
+
+const adds = document.querySelector(".add");
+
+function newInputNumber(){
+    const numberBtns = document.querySelector(".btn-container")
+    numberBtns.addEventListener("click", (e) => {
+        let buttons = e.target.closest(".num-btn")
+
+        if(!buttons) return
+
+        displayScreen(buttons)
+
+            if(currentNum === null){
+                currentNum = buttons.textContent
+            }else{
+                currentNum += buttons.textContent
+            }
+
+            if(justCalculated){
+                result.textContent = currentNum;
+                justCalculated = false
+            }
+
+    })
+}
+
+function operatorPress(){
+    const operatorContainer = document.querySelector(".btn-container");
+    operatorContainer.addEventListener("click", (e) => {
+        operatorBtns = e.target.closest(".operator-btn");
+
+        if(!operatorBtns) return
+
+        if(previousNum === null){
+
+            currentToPrev();
+
+            operator = operatorBtns.textContent
+
+            currentNum = null
+
+            justCalculated = true
+
+            topResult.textContent = `${previousNum} ${operator}`
+
+
+        }else{
+            previousNum = operate(previousNum, currentNum, operator)
+
+            operator = operatorBtns.textContent
+
+            currentNum = null
+
+            justCalculated = true
+
+            topResult.textContent = `${previousNum} ${operator}`
+          
+        }
+    })
+}
+
+function displayScreen(btn){
+    if(result.textContent === "0"){
+        if(btn.textContent === "."){
+            result.textContent += "."
+        }else{
+            result.textContent = btn.textContent
+        }
+    }else{
+        result.textContent += btn.textContent
+    }
+}
+
+function currentToPrev(){
+    previousNum = +currentNum
+}
+
+function decimalBtn(){
+    const decimal = document.querySelector(".decimal");
+
+    decimal.addEventListener("click", (e)=>{
+        if(result.textContent.includes(".")){
+            e.preventDefault
+        }else{
+            result.textContent += e.target.textContent
+            currentNum += e.target.textContent
+        }      
+    })
+    
+}
+
+const clears = document.querySelector(".clear")
+
+function clearBtn(){
+    clears.addEventListener("click", ()=>{
+        clearNumber();  
+    })
+}
+
+
+function clearNumber(){
+    currentNum = null;
+    previousNum = null;
+    operator = null;
+    topResult.textContent = "";
+    result.textContent = 0;
+}
+
+
+const equals = document.querySelector(".equalTo")
+function equalBtn(){
+    equals.addEventListener("click", ()=> {
+
+        if(previousNum === null || currentNum === null){
+            return
+        }
+        justCalculated = true
+        result.textContent = operate(previousNum, currentNum, operator)
+
+        topResult.textContent = `${previousNum} ${operator} ${currentNum} =`
+    })
+}
+
+
+newInputNumber();
+operatorPress();
+clearBtn();
+equalBtn();
+decimalBtn();
+
